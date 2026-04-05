ee6f598adedd30e54a475216702ed3ebaecef5ad
techfaiyaz5 <faiyazkhanmoyal07860@gmail.com>
2026-04-06 00:57:14 +0530

    changes-tages

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Welcome To the world.
           </li>
         </ol>
 
